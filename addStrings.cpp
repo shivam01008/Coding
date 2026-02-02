58cb8f682241798ca855c37ebb9a69a83e8fb99d
shivam01008 <shivamashokshukla09@gmail.com>
2026-02-02 23:26:12 +0530

    day6

---
+++ addStrings.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string addStrings(string num1, string num2) {
+    int i = num1.size() - 1;
+    int j = num2.size() - 1;
+    int carry = 0;
+    string result = "";
+
+    while (i >= 0 || j >= 0 || carry) {
+        int sum = carry;
+
+        if (i >= 0) sum += num1[i--] - '0';
+        if (j >= 0) sum += num2[j--] - '0';
+
+        carry = sum / 10;
+        result += (sum % 10) + '0';
+    }
+
+    reverse(result.begin(), result.end());
+    return result;
+}
